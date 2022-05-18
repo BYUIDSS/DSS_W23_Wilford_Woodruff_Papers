41dfda2dc1568a46cbddb1e1874070448a116f44
hghamblin <hghamblin@gmail.com>
2022-05-18 14:31:05 -0600

    Create letter_analysis.py

---
+++ personal_folders/hghamblin/letter_analysis.py
@@ -0,0 +1,25 @@
+# %%
+import re
+import pandas as pd
+
+# %%
+with open("derived_data/letters.txt", encoding = 'utf-8') as letters_file:
+
+    text = letters_file.read()
+
+# %%
+name_pattern = r"\[\[.+|.+]\]" # NEEDS WORK
+
+names = re.findall(name_pattern, text)
+
+letters = re.split(name_pattern, text)
+letters.pop(0)
+
+letters_dict = {
+    'recipient': names, 
+    'letters': letters
+}
+
+letters_df = pd.DataFrame.from_dict(letters_dict)
+
+# %%

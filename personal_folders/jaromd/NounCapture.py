78a286984433c170e7e6e988cdd7a4d91e9a996b
Jarom Diaz <92468206+JaromPD@users.noreply.github.com>
2022-06-08 19:00:21 -0600

    Added comments to NounCapture.py

--- personal_folders/jaromd/NounCapture.py
+++ personal_folders/jaromd/NounCapture.py
@@ -4,6 +4,12 @@ with open('derived_data\journals.txt') as journals_file:
     text = journals_file.read()
 
 def get_nouns(text):
+
+    '''
+    get_nouns goes through the given text to find nouns formatted as [ FORMAL NOUN | Wilford Woodruff's original writing].
+    It then strips unneccesary items to just get the formal noun, and returns a list of a list of people and a list of places.
+    '''
+
     # Pattern to grab nouns formatted as "[[NOUN ...|"
     pattern = r"\[\[.+?\|"
     # People and place lists initialized
@@ -12,14 +18,19 @@ def get_nouns(text):
     # The nouns are captured from given text.
     captured = re.findall(pattern, text)
 
-    # The nouns are cleaned of brackets and 
+    # The nouns are cleaned of brackets and the pipe symbol and split into people and places.
     for noun in captured:
+        # Unneeded symbols are stripped.
         clean_noun = noun.strip("[|")
+        # Nouns with commas are seperated.
         if "," in clean_noun:
+            # If they have the ", .b" the noun is a person but has a birthdate.
             if ", b." in clean_noun:
                 clean_people.append(clean_noun)
+            # If it doesn't it is a place.
             else:
                 clean_place.append(clean_noun)
+        # If there's no comma, it's a person.
         else:
             clean_people.append(clean_noun)
     

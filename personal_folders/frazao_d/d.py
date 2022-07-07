213b027cea31b2a63af2fd645921804d102b54d4
dayanjr.github.io <91074122+dayanjr@users.noreply.github.com>
2022-07-06 18:59:52 -0600

    update

--- personal_folders/frazao_d/d.py
+++ personal_folders/frazao_d/d.py
@@ -1,7 +1,33 @@
+import matplotlib.pyplot as plt
 clean_text_list = []
 with open('derived_data/journals.txt') as journals_file:
  text = journals_file.read()
 a1 = text
-def cleaning_data(text):
- a2 = text.replace('[[','').replace(']]','').replace('[figure','').replace(']','')
- return a2
+x = text.count("Brigham Young Jr")
+print(f"{x}")
+#d#ef cleaning_data(text):
+# #a2 = text.replace('[[','').replace(']]','').replace('[figure','').replace(']','')
+# #return a2
+
+# x-coordinates of left sides of bars 
+left = [1, 2,]
+  
+# heights of bars
+height = [9000, 34000,]
+  
+# labels for bars
+tick_label = ['places', 'persons']
+  
+# plotting a bar chart
+plt.bar(left, height, tick_label = tick_label,
+        width = 0.5, color = ['red', 'green'])
+  
+# naming the x-axis
+plt.xlabel('x - axis')
+# naming the y-axis
+plt.ylabel('y - axis')
+# plot title
+plt.title('Persons x places')
+  
+# function to show the plot
+plt.show()

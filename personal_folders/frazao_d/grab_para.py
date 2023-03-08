dbcd44920eb09b6d2c3728eba85e1614a1c98076
dayanjr <dayanfrazao@byui.edu>
2023-03-08 12:51:46 -0700

    change

---
+++ personal_folders/frazao_d/grab_para.py
@@ -0,0 +1,77 @@
+text = '''June 10, 1862 ~ Tuesday
+
+[FIGURE] 10 President [[Brigham Young|Young]] has ordered the raising of the foundation of the
+[[Salt Lake City Temple, Salt Lake City, Salt Lake County, Utah Territory|temple]] in consequence of the stones many of them having been put 
+upon spawls or cobble stones & not laid solid which he did not
+find out untill after it was done the workmen are taking up 
+the large top stones to day and they have taken out our depo-
+sit of records coin &c which we deposited in the foundation 
+which I never Expected to live to see taken out when I saw
+it deposited
+1862
+* On Saturday the Afternoon I met at the [[City Hall, Great Salt Lake City, Great Salt Lake County, Utah Territory|City Hall]] with Jeter 
+Clinton & [[Henry J. Doremus|H. I. Doremus]] several of the Doctors for Examinatin
+Preparatory to therier geting a license. we first Examined
+Dr [[John Wiseman|Wiseman]] He said Gout was High Rheumatism, for the
+Gout I no nothing better than Morphine or Opium and
+the Oil of Galvanium. For Inflamitory rheumatism I would
+give Tartar emetic medicine. For the diyspepsia I would give
+opening medicine & hops or strong beer x For gravel I would
+give Gensing ^GUaiacom^ & lime water. x For white swelling I use a poultice
+of Mandrake in its natural state that purifies the blood and
+sometimes I use it in medicine x for Lung fever I use Pyroligneous 
+acid and Gensing for a bad case I would use calomel & Mercury
+as nothing Els will reach the liver I give 5 or 6 grains. x for the
+Erysipelas I would use Lime water and calomel as a wash, and
+I would give cream of Tarter and Rhubarb To soften down
+what do you do for cancer's "cancears are caused in women 
+by suckling children to long in Men by Extreme case of scrofula
+I use Calomel & Opeum Externally and a plaster of Rosin
+and Olive Oil and a tincture of Gaenstian and Rhubarb
+In a difficult case of childbirth I would use Oil to soften the
+parts. If I had a shoulder presentation I should put it back
+when the child is born I sever the Naval Cord about 2 1/2 inches
+from the body. In case of Broaken breast I use Hazel nut oil
+any kind of spirits is injurious in such cases, for cake breast
+3 tablespoonfuls of Linseed meal 1/2 tablespoonful of mustard.
+For the cramp colic I would give Brandy and water or a
+Pill of Rhubarb & Gamboge a warm Bath will take it away
+For Forostbit I would give Turpentine with 1/4 vinegar
+
+Dr [[John Lewis Dunyon|Dunion]] said I believe all deseases is caused by cold, then
+there is an obstruction somewhare if the Lungs are the most
+susceptible to the cold then the fever will settle there. I give Lobelia
+Squills Narvine Horehound and many other things I use rhubarb
+seneca poke root blue flag root and the warm bath. For Inflamatory
+Rheumatism. I give Rhubarb and other cathartic medicine
+I use a Bath of sulpher, catnip, sage & other herbs for Erysipelus
+1862
+that I have had any thing to do with it the men who have 
+bossed the laying of those stones are fools or consummit rascals
+many other remarks were made upon the subject
+
+June 2, 1862 ~ Monday
+
+2d I spent most of the day about the water ditches, the City Creek
+was doing much damage & about 60 men turned out & opened a
+New ditch above brother Kimballs well.
+
+June 3, 1862 ~ Tuesday
+
+3d I spent the fore part of the day in the office Journalizing
+the afternoon at home
+
+June 4, 1862 ~ Wednesday
+
+4 I spent the forenoon at home watering the afternoon in the 
+office compiling History.
+
+June 5, 1862 ~ Thursday'''
+
+# Python program to illustrate
+# Matching regex objects
+# with multiple Groups with the Pipe
+import re
+heroRegex = re.compile (r'Batman|Tina Fey')
+mo1 = heroRegex.search('Batman and Tina Fey.')
+print(mo1.group())
